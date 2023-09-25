d2d11f53cfdf8ebb765629c1bc072f00f137c0a3
AndreMigz <andre@bootyard.com>
2023-09-25 12:57:41 +0800

    [FrontEnd] Scoring Table Layout

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,3 +1,10 @@
 class Rating < ApplicationRecord
   belongs_to :rateable, polymorphic: true
+
+  EXPOSURE = [
+    "Talent",
+    "Jeans Wear",
+    "Sports Wear",
+    "Preliminary QA"    
+  ]
 end
