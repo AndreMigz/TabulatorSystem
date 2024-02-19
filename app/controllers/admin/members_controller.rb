3666925513dead4472f187a2ef9fc835a7d1ec03
AndreMigz <migzarre@gmail.com>
2024-02-18 22:19:09 -0800

    [Add] Added member update on admin dashboard

--- app/controllers/admin/members_controller.rb
+++ app/controllers/admin/members_controller.rb
@@ -1,24 +1,40 @@
 class Admin::MembersController < AdminController
     before_action :authenticate_user!, :allow_admin
+    before_action :set_member, only: [:update, :destroy]
 
     def index
       @members = User.where(admin_id: current_user.id)
     end
 
     def create
-      @member = User.new(member_params)
+      member = User.new(member_params)
 
-      if @member.save
+      if member.save
         redirect_to admin_members_path, notice: "Member Added"
       else
         redirect_to admin_members_path, error: "Failed to add member."
       end
     end
 
+    def update
+      if @member.update(member_params)
+        redirect_to admin_members_path, notice: "Member Updated Successfully"
+      else
+        redirect_to admin_members_path, error: "Failed to update member."
+      end
+    end
+
     private
 
     def member_params
-      params.require(:user).permit(:admin_id, :email, :password, :password_confirmation)
+      params.require(:user).permit(:id, :admin_id, :email, :password, :password_confirmation)
+    end
+
+    def set_member
+      @member = User.find_by(id: params[:id])
+      Rails.logger.info(@member)
+
+      redirect_to admin_members_path, error: "Member not found." unless @member
     end
 
     def allow_admin
