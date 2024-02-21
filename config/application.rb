887f25f9705a4464cd2468c7458d705c89834a62
AndreMigz <migzarre@gmail.com>
2024-02-21 14:58:41 +0800

    [Add] Setup Environment Variables

--- config/application.rb
+++ config/application.rb
@@ -6,8 +6,16 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module RailsRuby3
+module TabularTrack
   class Application < Rails::Application
+    # Set local environment variables
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exist?(env_file)
+    end
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
